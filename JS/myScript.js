25c01938e7e30f6e1bafa8d6531c467b31250bb9
KidPercy <mthimkhulupercy3@mail.com>
2020-10-23 07:31:28 +0200

    more blogs added, Wireframe and style guide done.

--- JS/myScript.js
+++ JS/myScript.js
@@ -2,5 +2,4 @@ function onClickMenu(){
 	document.getElementById("menu").classList.toggle("change");
 	document.getElementById("Mobilenav").classList.toggle("change");
 	
-	//document.getElementById("menu-bg").classList.toggle("change-bg");
 }
