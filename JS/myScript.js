4a634bd355edad8a373d5d7963183332fc597318
KidPercy <mthimkhulupercy3@mail.com>
2020-11-09 22:10:42 +0200

    illustration page setup

--- JS/myScript.js
+++ JS/myScript.js
@@ -30,9 +30,16 @@ function topFunction() {
 
 
 
-/*$("div").animate({
-    left: '250px',
-    opacity: '0.5',
-    height: '150px',
-    width: '150px'
-  });*/
+//Gallery Tab
+function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
